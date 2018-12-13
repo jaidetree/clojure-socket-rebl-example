7e3d35f63e3eec47dcdde59c51c4d4a8475c5559
Jay Zawrotny <jayzawrotny@gmail.com>
2018-12-13 02:38:18 -0500

    Reused clojure.core.server/repl-init and repl-read functions.

--- src/clojure_socket_rebl_example/repl.clj
+++ src/clojure_socket_rebl_example/repl.clj
@@ -1,12 +1,7 @@
 (ns clojure-socket-rebl-example.repl
   (:require [cognitect.rebl]
-            [clojure.main :as m]))
-
-(defn repl-init
-  "Initialize repl in user namespace and make standard repl requires."
-  []
-  (in-ns 'user)
-  (apply require m/repl-requires))
+            [clojure.main :as m]
+            [clojure.core.server :refer [repl-init repl-read]]))
 
 (defn repl-eval
  "Enhanced :eval hook for repl supporting rebl/submit."
@@ -14,17 +9,6 @@
   (let [output (eval input)]
     (cognitect.rebl/submit input output)
     output))
-
-(defn repl-read
-  "Enhanced :read hook for repl supporting :repl/quit."
-  [request-prompt request-exit]
-  (or ({:line-start request-prompt :stream-end request-exit}
-       (m/skip-whitespace *in*))
-      (let [input (read {:read-cond :allow} *in*)]
-        (m/skip-if-eol *in*)
-        (case input
-          :repl/quit request-exit
-          input))))
 
 (defn repl
   "REPL with predefined hooks for attachable socket server."
